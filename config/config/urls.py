edc97fc95f5bf6277ffe7c2dc3c7489b78d13606
mahdi niknejad <mmahdiniknejad@gmail.com>
2022-10-30 15:07:02 +0330

    start project

---
+++ config/config/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('/auth', include('authentication.urls', namespace='auth')),
+    path('', include('general.urls', namespace='general')),
+]
