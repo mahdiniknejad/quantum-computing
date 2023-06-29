d261d963880a947e37e81e81b33fab20c839d424
Mahdi Niknejad <mmahdiniknejad@gmail.com>
2023-06-29 16:48:29 +0330

    debug TypeError: AnonymousUser

--- config/files/views.py
+++ config/files/views.py
@@ -3,6 +3,7 @@ import random
 from subprocess import PIPE, STDOUT, run
 from django.http import JsonResponse
 from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
 from rest_framework import viewsets, permissions, authentication
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -23,7 +24,7 @@ class FileViewSet(viewsets.ModelViewSet):
 	serializer_class = FileSerializer
 
 	def get_queryset(self):
-		if self.request.user:
+		if self.request.user is not AnonymousUser:
 			return self.request.user.file_set.all()
 		else:
 			return None
