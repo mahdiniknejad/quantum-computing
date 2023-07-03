7e8da67c5d4796714e17ca7f08261c9f4d282166
Mahdi Niknejad <mmahdiniknejad@gmail.com>
2023-07-03 21:23:11 +0330

    add CSRF_COOKIE_SECURE to settings

--- config/files/serializers.py
+++ config/files/serializers.py
@@ -5,17 +5,21 @@ from .models import File, Image
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
-        fields = '__all__'
-        read_only_fields = ['user', 'executed']
+        fields = "__all__"
+        read_only_fields = ["user", "executed"]
 
     def create(self, validated_data):
-        user = self.context['request'].user
-        validated_data['user'] = user
+        user = self.context["request"].user
+        validated_data["user"] = user
         return super().create(validated_data)
 
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
-        fields = '__all__'
-        read_only_fields = ['image', 'file']
+        fields = "__all__"
+        read_only_fields = ["image", "file"]
+
+
+class PackageInstallerSerializer(serializers.Serializer):
+    package = serializers.CharField(required=True)
--- config/files/views.py
+++ config/files/views.py
@@ -14,57 +14,72 @@ from .modules import check_some_codes
 
 
 def out(command):
-    result = run(command, stdout=PIPE, stderr=STDOUT, universal_newlines=True, shell=True)
+    result = run(
+        command, stdout=PIPE, stderr=STDOUT, universal_newlines=True, shell=True
+    )
     return result.stdout
 
 
 class FileViewSet(viewsets.ModelViewSet):
-	permission_classes = (permissions.IsAuthenticated,)
-	authentication_classes = (authentication.TokenAuthentication, )
-	serializer_class = FileSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (authentication.TokenAuthentication,)
+    serializer_class = FileSerializer
 
-	def get_queryset(self):
-		if not isinstance(self.request.user, AnonymousUser):
-			return self.request.user.file_set.all()
-		else:
-			return None
+    def get_queryset(self):
+        if not isinstance(self.request.user, AnonymousUser):
+            return self.request.user.file_set.all()
+        else:
+            return None
 
-	@swagger_auto_schema(methods=['POST'])
-	@action(methods=['post'], detail=True)
-	def execute(self, request, pk, *args, **kwargs):
-		
-		file = File.methods.filter(id=pk).first()
-		code = file.code
-		code, pic = check_some_codes(code)
-		temp_name = str(random.randint(10000, 99999)) + '.py'
-		file_path = str(settings.BASE_DIR) + "/code/" + temp_name
+    @swagger_auto_schema(methods=["POST"])
+    @action(methods=["post"], detail=True)
+    def execute(self, request, pk, *args, **kwargs):
+        file = File.methods.filter(id=pk).first()
+        code = file.code
+        code, pic = check_some_codes(code)
+        temp_name = str(random.randint(10000, 99999)) + ".py"
+        file_path = str(settings.BASE_DIR) + "/code/" + temp_name
 
-		os.system(f"touch {file_path}")
-		os.system(f"echo \"{code}\" > {file_path}")
-		res = out(f"{settings.BASE_DIR}/./manage.py shell < {file_path}")
-		res = '\n'.join(res.split('\n')[-5:])
-		os.system(f"rm {file_path}")
+        os.system(f"touch {file_path}")
+        os.system(f'echo "{code}" > {file_path}')
+        res = out(f"{settings.BASE_DIR}/./manage.py shell < {file_path}")
+        res = "\n".join(res.split("\n")[-5:])
+        os.system(f"rm {file_path}")
 
-		if os.path.isfile(pic):
-			Image.objects.create(image=pic.split('/')[-1], file=file)
+        if os.path.isfile(pic):
+            Image.objects.create(image=pic.split("/")[-1], file=file)
 
-		if res == '':
-			res = 'done'
+        if res == "":
+            res = "done"
 
-		return JsonResponse({"result": res}, status=200)
+        return JsonResponse({"result": res}, status=200)
 
-	@swagger_auto_schema(methods=['GET'])
-	@action(methods=['get'], detail=True)
-	def get_images(self, request, pk, *args, **kwargs):
-		file = File.methods.filter(id=pk).first()
-		return Response(ImageSerializer(file.image_set.all(), many=True).data, status=200)
+    @swagger_auto_schema(methods=["GET"])
+    @action(methods=["get"], detail=True)
+    def get_images(self, request, pk, *args, **kwargs):
+        file = File.methods.filter(id=pk).first()
+        return Response(
+            ImageSerializer(file.image_set.all(), many=True).data, status=200
+        )
 
 
 class ImageViewSet(viewsets.mixins.DestroyModelMixin, viewsets.GenericViewSet):
-	permission_classes = (permissions.IsAuthenticated,)
-	authentication_classes = (authentication.TokenAuthentication, )
-	serializer_class = ImageSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (authentication.TokenAuthentication,)
+    serializer_class = ImageSerializer
 
-	def get_queryset(self):
-		if not isinstance(self.request.user, AnonymousUser):
-			return Image.objects.filter(file__user=self.request.user)
+    def get_queryset(self):
+        if not isinstance(self.request.user, AnonymousUser):
+            return Image.objects.filter(file__user=self.request.user)
+
+
+class PackageInstallerAPIView(
+    viewsets.mixins.CreateModelMixin, viewsets.GenericViewSet
+):
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (authentication.TokenAuthentication,)
+    serializer_class = ImageSerializer
+
+    def get_queryset(self):
+        if not isinstance(self.request.user, AnonymousUser):
+            return Image.objects.filter(file__user=self.request.user)
