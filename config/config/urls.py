0cb84d50658cf53f9ab00cf2d165de07dad33d24
Mahdi Niknejad <mmahdiniknejad@gmail.com>
2023-08-08 21:24:36 +0330

    add urls to config

--- config/config/urls.py
+++ config/config/urls.py
@@ -7,28 +7,31 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Quantom Camputer",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
-   authentication_classes=[authentication.TokenAuthentication]
+    openapi.Info(
+        title="Snippets API",
+        default_version="v1",
+        description="Quantom Camputer",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    authentication_classes=[authentication.TokenAuthentication],
 )
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-
-
-    path('user/', include('users.urls', namespace='users')),
-    path('file/', include('files.urls', namespace='files')),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path(
+        "docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("user/", include("users.urls", namespace="users")),
+    path("file/", include("files.urls", namespace="files")),
+    path("docs/", include("documentation.urls", namespace="docs")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
