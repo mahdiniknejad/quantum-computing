66483b71875324d572250bbe0dd868608e4540d0
Mahdi Niknejad <mmahdiniknejad@gmail.com>
2023-08-08 20:51:49 +0330

    add doc model

--- config/documentation/admin.py
+++ config/documentation/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Doc
+
 
 # Register your models here.
+class DocAdmin(admin.ModelAdmin):
+    list_display = ["id"]
+
+
+admin.site.register(Doc, DocAdmin)
--- config/files/admin.py
+++ config/files/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import File, Image
 
 # Register your models here.
+
+
+class FileAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ["file__id"]
+
+
+admin.site.register(File, FileAdmin)
+admin.site.register(Image, ImageAdmin)
